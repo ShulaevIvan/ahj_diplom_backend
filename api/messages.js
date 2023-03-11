d9e66e0be8b745340f488b4123567033571c5f53
Shulaev Ivan <mail@ntdservice.ru>
2023-03-11 18:17:32 +0300

    fix a

--- api/messages.js
+++ api/messages.js
@@ -2,9 +2,10 @@ const database = require('../database/db');
 
 exports.getMessages = (ctx) => new Promise((resolve, reject) => {
     try {
+      console.log(database)
       const result = {
         status: 'ok',
-        timestamp: Number(new Date())
+        messsages: database.allData
       };
       resolve(result);
     } catch (err) {
